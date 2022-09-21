d0af89c8b327d00557c475fc3cfb9e8c276b1cca
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2022-09-21 22:51:50 +0530

    Assignment 2 self solved

--- day1_1/01-starting-setup/src/components/Expenses/Expenses.js
+++ day1_1/01-starting-setup/src/components/Expenses/Expenses.js
@@ -4,15 +4,14 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props){
-    const addFilterHandler=(e)=> {
-        
+    const addFilterHandler=(dataReceived)=> {
+        console.log("in expenses.js");
+        console.log(dataReceived);
     }
     return(
         <div>
-            <div>
-                <ExpensesFilter onAddFilter={addFilterHandler}/>
-            </div>
         <Card className="expenses">
+        <ExpensesFilter onAddFilter={addFilterHandler}/>
             <ExpenseItem name={props.array[0].title} date={props.array[0].date} price={props.array[0].amount}></ExpenseItem>
             <ExpenseItem name={props.array[1].title} date={props.array[1].date} price={props.array[1].amount}></ExpenseItem>
             <ExpenseItem name={props.array[2].title} date={props.array[2].date} price={props.array[2].amount}></ExpenseItem>
--- day1_1/01-starting-setup/src/components/Expenses/ExpensesFilter.js
+++ day1_1/01-starting-setup/src/components/Expenses/ExpensesFilter.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './ExpensesFilter.css';
 
-const ExpensesFilter = () => {
+const ExpensesFilter = (props) => {
+  const [setFilterVal, setFilterFunction] = useState('');
+  const onValueSelected=(e)=>{
+    setFilterFunction(e.target.value);
+  }
+  props.onAddFilter(setFilterVal);
+  
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select>
+        <select onChange={onValueSelected}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
