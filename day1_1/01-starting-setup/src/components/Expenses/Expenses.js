5fe48682fda5297d69e1b9c79ebcef6fc045025c
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2022-09-22 16:52:04 +0530

    adding unique keys

--- day1_1/01-starting-setup/src/components/Expenses/Expenses.js
+++ day1_1/01-starting-setup/src/components/Expenses/Expenses.js
@@ -21,6 +21,7 @@ const Expenses = (props) => {
         />
         {props.myArray.map((expense) => (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
