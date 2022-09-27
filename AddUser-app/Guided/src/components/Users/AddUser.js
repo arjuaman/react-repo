1be04155b8c922cfc65e6e952adab3f732824eaf
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2022-09-28 00:59:20 +0530

    Adding state for storing changed data

--- AddUser-app/Guided/src/components/Users/AddUser.js
+++ AddUser-app/Guided/src/components/Users/AddUser.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) =>{
+    const [enteredUsername, setenteredUsername] = useState('');
+    const [enteredAge, setenteredAge] = useState('');
+
     const addUserHandler = (e) =>{
         e.preventDefault();
+        console.log(enteredUsername, enteredAge);
+    }
+
+    const userNameChangeHandler = (e) => {
+        setenteredUsername(e.target.value);
+    }
+    
+    const ageChangeHandler = (e) => {
+        setenteredAge(e.target.value);
     }
 
     return (
         <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" />
+            <input id="username" type="text" onChange={userNameChangeHandler}/>
             <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number" />
+            <input id="age" type="number" onChange={ageChangeHandler} />
             <Button type="submit">Add User</Button>
         </form>
         </Card>
