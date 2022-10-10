6ff14e270596c4d34b6d080ec82a9bcf61443899
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2022-10-10 18:29:57 +0530

    Creating and providing

--- useEffect_start/src/App.js
+++ useEffect_start/src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContent from './store/auth-content';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -27,13 +28,13 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    <AuthContent.Provider>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContent.Provider>
   );
 }
 
---
+++ useEffect_start/src/store/auth-content.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AuthContent = React.createElement({
+    isLoggedIn: false
+});
+
+export default AuthContent;
