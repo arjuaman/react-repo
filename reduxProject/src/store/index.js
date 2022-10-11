5589172e3f9cb06e2686d67dcfcd3b7f23646f8d
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2022-10-11 14:15:56 +0530

    for redux specific

---
+++ reduxProject/src/store/index.js
@@ -0,0 +1,26 @@
+// const redux = require('redux');
+import {createStore} from 'redux';
+
+const reducerFunc = (state={counter:0}, action) => {
+    if(action.type==='increment'){
+        return {counter: state.counter + 1}
+    }
+    if(action.type==='decrement'){
+        return {counter: state.counter - 1}
+    }
+    return state;
+}
+
+const store = createStore(reducerFunc);
+
+// const subscriberFunc = () => {
+//     const latestState = store.getState();
+//     console.log(latestState);
+// }
+
+// store.subscribe(subscriberFunc);
+
+// store.dispatch({type:'increment'});
+// store.dispatch({type:'decrement'});
+
+export default store;
