7578265409757067b7a20c1471ab14c0638efd4d
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2022-10-14 01:29:54 +0530

    Managing state & TS

--- reactWithTs/react-ts/src/App.tsx
+++ reactWithTs/react-ts/src/App.tsx
@@ -1,16 +1,23 @@
+import { useState } from 'react';
+
 import './App.css';
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
-  const todos=[
-    new Todo('Hello from todoItem'),
-    new Todo('Hello again')
-  ];
+  // const todos=[
+  //   new Todo('Hello from todoItem'),
+  //   new Todo('Hello again')
+  // ];
+  const [todos, setTodos] = useState<Todo[]>([]);
   
   const addTodoHandler = (text: string) => {
-    
+    const newTodo = new Todo(text);
+    setTodos((prevTodos)=>{
+      return [...prevTodos, newTodo];
+      // can also be prevTodos.concat(newTodo);
+    });
   };
 
   return (
