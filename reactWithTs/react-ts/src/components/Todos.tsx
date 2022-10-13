de8b8d5b805f9a9ff2fc3c9d597a675fcfa41452
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2022-10-14 02:04:43 +0530

    Removing a todo by building a prop chain and binding

--- reactWithTs/react-ts/src/components/Todos.tsx
+++ reactWithTs/react-ts/src/components/Todos.tsx
@@ -3,11 +3,11 @@ import Todo from '../models/todo';
 import TodoItem from './TodoItem';
 import classes from "./Todos.module.css"
 
-const Todos:React.FC<{items:Todo[]}> = (props) =>{
+const Todos:React.FC<{items:Todo[]; onRemoveTodo:(id: string)=>void}> = (props) =>{
     return(
         <ul className={classes.todos}>
             {props.items.map((item) => <li key={item.id}>
-                <TodoItem text={item.text} />
+                <TodoItem text={item.text} onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}/>
             </li>)}
         </ul>
     );
