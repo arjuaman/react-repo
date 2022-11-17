32c86d5bc1a5e3ee14ef75a4488ede98412cedcf
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2022-11-18 00:03:56 +0530

    Managing cart opening, closing and backdrop closing

--- foodOrderApp/src/components/Layout/Header.js
+++ foodOrderApp/src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import classes from './Header.module.css';
 import mealsImage from '../../assets/meals.jpg';
 import HeaderCartButton from "./HeaderCartButton";
 
-const Header = () => {
+const Header = (props) => {
     return(
         <React.Fragment>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <HeaderCartButton>Cart</HeaderCartButton>
+                <HeaderCartButton onClick={props.onShowCart}>Cart</HeaderCartButton>
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt="a table full of delicious food"></img>
