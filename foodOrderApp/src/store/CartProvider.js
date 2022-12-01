96516111f3a0724d6dab7a3af23233188db1c7ef
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2022-12-01 21:25:00 +0530

    adding a cart context

---
+++ foodOrderApp/src/store/CartProvider.js
@@ -0,0 +1,24 @@
+import CardContext from "./cart-context";
+
+const CartProvider = props => {
+    const addItemToCartHandler = item => {
+
+    };
+
+    const removeItemFromCartHandler = item => {
+
+    };
+
+    const cartContext = {
+        items: [],
+        totalAmount : 0,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler
+    };
+
+    return <CardContext.Provider value={cartContext}>
+        {props.children}
+    </CardContext.Provider>
+};
+
+export default CartProvider;
